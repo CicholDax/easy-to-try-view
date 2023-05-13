cc002250acd7db332c6a26363261111df36d7b66
noto-rexlabo\CicholDax <cicdax@gmail.com>
2023-05-14 02:23:23 +0900

    サンプル更新

--- Assets/ETTView/Sample/ReenactSample/TestReenactable.cs
+++ Assets/ETTView/Sample/ReenactSample/TestReenactable.cs
@@ -5,4 +5,23 @@ using UnityEngine;
 public class TestReenactable : Reenactable
 {
 	[SerializeField] string _textField;
+	[SerializeField] GameObject _testObjectField;
+
+	string _test;
+	GameObject _testgo;
+
+
+	public void Awake()
+	{
+		_test = "hoge";
+		_testgo = _testObjectField;
+	}
+
+	public void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.F3))
+		{
+			Debug.Log(_testgo.name);
+		}
+	}
 }
